cf0af0c06448e5bd7d3dacab6d646fd545203ed1
junhyuk0801 <junhyuk0801@gmail.com>
2023-04-09 01:12:44 +0900

    feat: add ws route to view log stream [Ongoing]

---
+++ application/services/logViewer_service.go
@@ -0,0 +1,8 @@
+package services
+
+type LogViewer struct {
+}
+
+func (l LogViewer) DescribeLogs(channel string, user string) {
+
+}
---
+++ interface/routes/logViewer_route.go
@@ -0,0 +1,11 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func LogViewerRoutes(a *fiber.App) {
+	route := a.Group("/logviewer")
+
+	re
+}
